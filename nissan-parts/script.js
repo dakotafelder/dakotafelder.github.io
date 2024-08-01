5994e2a52dcb77441e6b6b77f2ef49bd9cbdd14b
dakotafelder <hello@dakotafelder.com>
2024-08-01 00:05:10 -0500

    mobile breakpoint

--- nissan-parts/script.js
+++ nissan-parts/script.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
       "product": {
           "@media (min-width: 601px)": {
           "max-width": "calc(25% - 20px)",
-          "margin-left": "20px",
+          "margin-left": "0px",
           "margin-bottom": "50px"
           }
       },
@@ -78,94 +78,6 @@ document.addEventListener("DOMContentLoaded", function() {
       "button": "Buy CAD File"
       }
   },
-  "productSet": {
-      "styles": {
-      "products": {
-          "@media (min-width: 601px)": {
-          "margin-left": "-20px"
-          }
-      }
-      }
-  },
-  "modalProduct": {
-      "contents": {
-      "img": false,
-      "imgWithCarousel": true,
-      "button": false,
-      "buttonWithQuantity": true
-      },
-      "styles": {
-      "product": {
-          "@media (min-width: 601px)": {
-          "max-width": "100%",
-          "margin-left": "0px",
-          "margin-bottom": "0px"
-          }
-      },
-      "button": {
-          "font-weight": "bold",
-          "font-size": "16px",
-          "padding-top": "16px",
-          "padding-bottom": "16px",
-          ":hover": {
-          "background-color": "#000000"
-          },
-          "background-color": "#000000",
-          ":focus": {
-          "background-color": "#000000"
-          },
-          "border-radius": "6px"
-      },
-      "quantityInput": {
-          "font-size": "16px",
-          "padding-top": "16px",
-          "padding-bottom": "16px"
-      }
-      },
-      "text": {
-      "button": "Add to cart"
-      }
-  },
-  "option": {},
-  "cart": {
-      "styles": {
-      "button": {
-          "font-weight": "bold",
-          "font-size": "16px",
-          "padding-top": "16px",
-          "padding-bottom": "16px",
-          ":hover": {
-          "background-color": "#000000"
-          },
-          "background-color": "#000000",
-          ":focus": {
-          "background-color": "#000000"
-          },
-          "border-radius": "6px"
-      }
-      },
-      "text": {
-      "total": "Subtotal",
-      "button": "Checkout"
-      }
-  },
-  "toggle": {
-      "styles": {
-      "toggle": {
-          "font-weight": "bold",
-          "background-color": "#000000",
-          ":hover": {
-          "background-color": "#000000"
-          },
-          ":focus": {
-          "background-color": "#000000"
-          }
-      },
-      "count": {
-          "font-size": "16px"
-      }
-      }
-  }
   },
       });
       });
